d4228c7907db052f42db1cc77b6c1ee5c04e834c
aminelebrini <aminelebrini4@gmail.com>
2026-01-17 00:52:56 +0100

    conexion db

--- src/Core/Data.php
+++ src/Core/Data.php
@@ -1,11 +1,46 @@
 <?php
 
-namespace Data;
+namespace Core;
 
+use PDO;
+use PDOException;
 
 class Data
 {
-    
+    private static ?Data $instance = null;
+    private PDO $connection;
+
+    public function __construct()
+    {
+        try{
+            $this->connection = new PDO(
+                "pgsql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_NAME']}",$_ENV['DB_USER'],$_ENV['DB_PASSWORD'],
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                ]
+            );
+
+            if($this->connection())
+            {
+                echo "wa33333";
+            }
+        }catch(PDOException $e)
+        {
+            echo $e->getMessage();
+        }
+    }
+    public static function getInstance(): Data
+    {
+        if (self::$instance === null) {
+            self::$instance = new Data();
+        }
+        return self::$instance;
+    }
+    public function connection()
+    {
+        return self::$connection;
+    }
 }
 
 ?>
--- src/public/index.php
+++ src/public/index.php
@@ -2,9 +2,17 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../Core/Router.php';
 use Core\Router;
+use Core\Data;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
 $router = new Router();
 
 $router->get('/', 'Controllers\\HomeController@index', 'visiteur');
 
 $router->generate_path();
+
+
+
+
